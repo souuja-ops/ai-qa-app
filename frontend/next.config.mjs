185bd4e8a4a473077daa928670233cf03f75e890
souuja-ops <1803385@students.kcau.ac.ke>
2025-06-03 00:33:17 +0300

    Fix: lightningcss

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // Completely disable lightningcss
-    css: false,
-  },
 };
 
 export default nextConfig;
